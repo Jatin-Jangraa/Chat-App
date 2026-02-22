9b4a1f161ddef80da3838745e95e1c1cf54a6560
Jatin-Jangraa <jatin012468@gmail.com>
2026-02-22 19:35:56 +0530

    update

--- server/dist/index.js
+++ server/dist/index.js
@@ -18,7 +18,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // 🌐 Middlewares
 app.use(cors({
-    origin: "*",
+    origin: "https://chat-app-37ky.onrender.com/",
     credentials: true,
 }));
 app.use(cookieParser());
